d31b5c8f0423309bf934ea60f7c6e48411c535ae
DeadlyEmbrace <ralphviktor@ninetaillabs.com>
2021-09-18 16:05:21 +0200

    feat: FileSavePicker wrapper. Closes #13
    - Added wrapper to set the Window that owns the picker
    - Added wrapper for FileSavePicker to allow saving a single file

---
+++ VaraniumSharp.WinUI/Interfaces/Pickers/IPickerWrapper.cs
@@ -0,0 +1,24 @@
+﻿using System.Collections.Generic;
+using System.Threading.Tasks;
+using Windows.Storage;
+
+namespace VaraniumSharp.WinUI.Interfaces.Pickers
+{
+    /// <summary>
+    /// Wrapper for Pickers
+    /// </summary>
+    public interface IPickerWrapper
+    {
+        #region Public Methods
+
+        /// <summary>
+        /// Pick a single file with a specific type
+        /// </summary>
+        /// <param name="fileTypes">Dictionary containing file type name and extensions</param>
+        /// <param name="suggestedFilename">Suggested name for the file. Pass null or empty to leave empty</param>
+        /// <returns>StorageFile from the pick</returns>
+        Task<StorageFile> PickSaveFileAsync(KeyValuePair<string, List<string>> fileTypes, string? suggestedFilename);
+
+        #endregion
+    }
+}
---
+++ VaraniumSharp.WinUI/Pickers/PickerWrapper.cs
@@ -0,0 +1,60 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Threading.Tasks;
+using Windows.Storage;
+using Windows.Storage.Pickers;
+using VaraniumSharp.Attributes;
+using VaraniumSharp.WinUI.Interfaces.Pickers;
+
+namespace VaraniumSharp.WinUI.Pickers
+{
+    /// <summary>
+    /// Wrapper class for Pickers
+    /// </summary>
+    [AutomaticContainerRegistration(typeof(IPickerWrapper))]
+    public class PickerWrapper : IPickerWrapper
+    {
+        #region Constructor
+
+        /// <summary>
+        /// DI Constructor
+        /// </summary>
+        public PickerWrapper(IOwnerWindow ownerWindow)
+        {
+            _ownerWindow = ownerWindow;
+        }
+
+        #endregion
+
+        #region Public Methods
+
+        /// <inheritdoc />
+        public async Task<StorageFile> PickSaveFileAsync(KeyValuePair<string, List<string>> fileTypes, string? suggestedFilename)
+        {
+            var savePicker = new FileSavePicker();
+            savePicker.FileTypeChoices.Add(fileTypes.Key, fileTypes.Value);
+
+            if (!string.IsNullOrEmpty(suggestedFilename))
+            {
+                savePicker.SuggestedFileName = suggestedFilename;
+            }
+
+            // See: https://github.com/microsoft/WindowsAppSDK/issues/467#issuecomment-901220636
+            var hwnd = WinRT.Interop.WindowNative.GetWindowHandle(_ownerWindow.ParentWindow);
+            WinRT.Interop.InitializeWithWindow.Initialize(savePicker, hwnd);
+
+            return await savePicker.PickSaveFileAsync();
+        }
+
+        #endregion
+
+        #region Variables
+
+        /// <summary>
+        /// OwnerWindow instance
+        /// </summary>
+        private readonly IOwnerWindow _ownerWindow;
+
+        #endregion
+    }
+}
